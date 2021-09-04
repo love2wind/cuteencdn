30d84b8c6e72b3056a7dc30783fdad33b13f2792
love2wind <love2wind@gmail.com>
2021-09-04 17:10:35 +0800

    edit footerdown style

--- spzac/footerdown.php
+++ spzac/footerdown.php
@@ -3,14 +3,16 @@
     <div class="footer-fav">
       <div class="container">
         <div class="fl site-info">
-          <h2><a href="https://chenyu.me/" target="_blank">錯愛涅槃</a></h2>
+          <h2><a href="https://love2wind.cn/" target="_blank">錯愛涅槃</a></h2>
           <div class="site-p">
-					<script  type="text/javascript">
-					/*var colorStr="";
+              <p>LOVE2WIND.CN【白茶清欢无别事，我在等风也等你】</p>
+              <p>风风雨雨中，恭迎本站第<?php echo theAllViews();?>位来访的小伙伴。</p>
+					<!--<script  type="text/javascript">
+					var colorStr="";
 					var randomArr=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
 					for(var i=0;i<6;i++){
 						colorStr+=randomArr[Math.ceil(Math.random()*(15-0)+0)];
-					}*/
+					}
 					var now =new Date(); 
 					function StorageTime() { 
 						var grt= new Date("11/27/2011 00:00:00");//时间格式：月/日/年  时/分/秒
@@ -23,15 +25,12 @@
 						if(String(minutes).length ==1 ){minutes = "0" + minutes;}
 						seconds = Math.floor((now - grt ) / 1000 - (24 * 60 * 60 * Math.floor((now - grt ) / 1000 / 60 / 60 / 24)) - (60 * 60 * hours) - (60 * minutes)); 
 						if(String(seconds).length ==1 ){seconds = "0" + seconds;}
-					/*	if(years!=0){var outputtime=+years+"年"+days+"天"+hours+"时"+minutes+"分"+seconds+"秒";}else{var outputtime=+days+"天"+hours+"时"+minutes+"分"+seconds+"秒";}*/
-						if(years!=0){var outputtime=+years+"年"+days+"天"+hours+"时";}else{var outputtime=+days+"天"+hours+"时"+minutes+"分";}
-						/*document.getElementById("span_dt_dt").style.color="#"+colorStr;*/
-						document.getElementById("span_dt_dt").innerHTML = outputtime;
+						if(years!=0){var outputtime=+years+"年"+days+"天"+hours+"时"+minutes+"分"+seconds+"秒";}else{var outputtime=+days+"天"+hours+"时"+minutes+"分"+seconds+"秒";}
+						document.getElementById("siteday").style.color="#"+colorStr;
+						document.getElementById("siteday").innerHTML = outputtime;
 					} 
 					setInterval("StorageTime()",250);
-					</script>
-              <p>Love2wind.CN【白茶清欢无别事，我在等风也等你】</p>
-              <p>风风雨雨<span id ="span_dt_dt"></span>，你是第<?php echo theAllViews();?>位相遇的小伙伴</p>
+					</script>-->
           </div>
         </div>
         <div class="fr site-fav">
